6c26dbfb1b27e7925c8e4085d5cf698ccbf0b4f1
donsiamese <donsiamese@gmail.com>
2024-07-31 17:58:36 +0200

    remove redundant comps

--- src/Ui/ContentContainer.jsx
+++
@@ -1,9 +0,0 @@
-const ContentContainer = ({ children, cusromClassName, fistBlock }) => {
-  return fistBlock ? (
-    <div className={`text-left ${cusromClassName}`}>{children}</div>
-  ) : (
-    <div className={`bg-black/50 text-left py-8 px-10  ${cusromClassName}`}>{children}</div>
-  )
-}
-
-export default ContentContainer
