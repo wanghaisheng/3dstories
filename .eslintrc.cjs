9c5256091b8494bb55d6114928ed9e938a3b8b29
donsiamese <donsiamese@gmail.com>
2024-08-06 11:03:02 +0200

    refactoring code helpers

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,16 +6,16 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+
+  plugins: ['react', 'react-hooks', 'react-refresh', 'jsx-a11y', 'prettier'],
   rules: {
     'react/jsx-no-target-blank': 'off',
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    'prettier/prettier': 'error',
   },
 }
