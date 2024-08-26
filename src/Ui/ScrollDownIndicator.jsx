a4a8f797b5d3b2ca5d6d79ff53028eb15df45f1c
donsiamese <donsiamese@gmail.com>
2024-08-26 13:23:41 +0200

    add Scroll Indicator

---
+++ src/Ui/ScrollDownIndicator.jsx
@@ -0,0 +1,11 @@
+import './ScrollDownIndicator.css'
+
+const ScrollDownIndicator = () => {
+  return (
+    <div className={`ScrollDownIndicator flex items-center justify-center`}>
+      <span></span>
+    </div>
+  )
+}
+
+export default ScrollDownIndicator
