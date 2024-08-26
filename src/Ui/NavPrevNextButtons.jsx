d943d4dd35bde53018f228ec4bf9f9915f361bcc
donsiamese <donsiamese@gmail.com>
2024-08-26 13:26:32 +0200

    scroll indicator animation

--- src/Ui/NavPrevNextButtons.jsx
+++ src/Ui/NavPrevNextButtons.jsx
@@ -5,6 +5,7 @@ import { useMediaQuery } from 'react-responsive'
 import { useScrollStore } from '../components/ScrollManager'
 import { useSpring, a } from '@react-spring/web'
 import { RoutesUsingButtons } from '../constants'
+import ScrollDownIndicator from './ScrollDownIndicator'
 
 const NavPrevNextButtons = ({ threshold = 0, thresholdGoUp = 1, scrollToTop }) => {
   const isVisible = useRef(true)
@@ -18,7 +19,6 @@ const NavPrevNextButtons = ({ threshold = 0, thresholdGoUp = 1, scrollToTop }) =
   const currentRouteIndex = RoutesUsingButtons.findIndex(route => route.pathname === location.pathname)
 
   const disableNextButton = currentRouteIndex === -1 || currentRouteIndex === RoutesUsingButtons.length - 1
-  // const disableNextButton = currentRouteIndex === 0
   const disablePreviousButton = currentRouteIndex < 1
 
   const handleNext = () => {
@@ -34,6 +34,10 @@ const NavPrevNextButtons = ({ threshold = 0, thresholdGoUp = 1, scrollToTop }) =
     }
   }
 
+  const [scrollIndicatorStyles, apiIndicatorStyles] = useSpring(() => ({
+    opacity: initiallScrollRatioRef.current > threshold ? 0 : 1
+  }))
+
   const [styles, api] = useSpring(() => ({
     opacity: initiallScrollRatioRef.current > threshold ? 0 : 1,
     y: initiallScrollRatioRef.current > threshold ? -10 : 0
@@ -44,9 +48,9 @@ const NavPrevNextButtons = ({ threshold = 0, thresholdGoUp = 1, scrollToTop }) =
     y: initiallScrollRatioRef.current > thresholdGoUp ? 10 : 0
   }))
 
-  const [{ x }, apiDisableButtons] = useSpring(() => {
-    0
-  })
+  const [{ x }, apiDisableButtons] = useSpring(() => ({
+    x: 0
+  }))
 
   useEffect(() => {
     apiDisableButtons.start({
@@ -58,29 +62,42 @@ const NavPrevNextButtons = ({ threshold = 0, thresholdGoUp = 1, scrollToTop }) =
     () =>
       useScrollStore.subscribe(state => {
         if (state.scrollRatio > threshold && isVisible.current) {
+          apiIndicatorStyles.start({
+            opacity: 0
+          })
+        } else if (state.scrollRatio <= threshold && !isVisible.current) {
+          apiIndicatorStyles.start({
+            opacity: 1
+          })
+        }
+        if (state.scrollRatio > threshold && isVisible.current) {
           isVisible.current = false
           api.start({
             opacity: 0,
-            y: -10
+            y: -10,
+            pointerEvents: 'none'
           })
         } else if (state.scrollRatio <= threshold && !isVisible.current) {
           isVisible.current = true
           api.start({
             opacity: 1,
-            y: 0
+            y: 0,
+            pointerEvents: 'auto'
           })
         }
         if (state.scrollRatio < thresholdGoUp && isVisibleScrollUp.current) {
           isVisibleScrollUp.current = false
           apiScrollUp.start({
             opacity: 0,
-            y: -10
+            y: -10,
+            pointerEvents: 'none'
           })
         } else if (state.scrollRatio >= thresholdGoUp && !isVisibleScrollUp.current) {
           isVisibleScrollUp.current = true
           apiScrollUp.start({
             opacity: 1,
-            y: 0
+            y: 0,
+            pointerEvents: 'auto'
           })
         }
       }),
@@ -90,30 +107,35 @@ const NavPrevNextButtons = ({ threshold = 0, thresholdGoUp = 1, scrollToTop }) =
   console.info('[NavPrevNextButtons] currentRouteIndex:', currentRouteIndex, RoutesUsingButtons, location.pathname)
 
   return (
-    <a.div style={{ x }} className="NavPrevNextButtons z-[3] absolute top-0 bottom-0">
-      {pathname !== '/about' ? (
-        <div className="fixed w-screen" style={{ top: isBigScreen ? 'calc(50% - 130px)' : '15%' }}>
-          <a.div style={styles} className="absolute  flex xl:flex-col md:flex-row left-[1rem] lg:left-[5rem] ">
-            <a onClick={handlePrevious} disabled={disablePreviousButton}>
-              <CircleButton size={isBigScreen ? 120 : 80} rotate={180} />
-            </a>
-            <a className="relative" onClick={handleNext} disabled={disableNextButton}>
-              <CircleButton size={isBigScreen ? 120 : 80} className="xl:mt-5 xl:ml-0 ml-5" />
+    <div className="button-control-group">
+      <a.div style={scrollIndicatorStyles}>
+        <ScrollDownIndicator />
+      </a.div>
+      <a.div style={{ x }} className="NavPrevNextButtons z-[3] absolute top-0 bottom-0">
+        {pathname !== '/about' ? (
+          <div className="fixed w-screen" style={{ top: isBigScreen ? 'calc(50% - 130px)' : '15%' }}>
+            <a.div style={styles} className="absolute  flex xl:flex-col md:flex-row left-[1rem] lg:left-[5rem] ">
+              <a onClick={handlePrevious} disabled={disablePreviousButton}>
+                <CircleButton size={isBigScreen ? 120 : 80} rotate={180} />
+              </a>
+              <a className="relative" onClick={handleNext} disabled={disableNextButton}>
+                <CircleButton size={isBigScreen ? 120 : 80} className="xl:mt-5 xl:ml-0 ml-5" />
+              </a>
+            </a.div>
+          </div>
+        ) : null}
+        <div
+          className="go-to-top z-20 fixed flex flex-col"
+          style={{ bottom: 'calc(15% + 50px)', left: isBigScreen ? '5rem' : 'calc(50% - 40px)' }}
+        >
+          <a.div style={stylesScrollUp}>
+            <a onClick={scrollToTop}>
+              <CircleButton size={isBigScreen ? 120 : 80} rotate={-90} />
             </a>
           </a.div>
         </div>
-      ) : null}
-      <div
-        className="go-to-top z-20 fixed flex flex-col"
-        style={{ bottom: 'calc(15% + 50px)', left: isBigScreen ? '5rem' : 'calc(50% - 40px)' }}
-      >
-        <a.div style={stylesScrollUp}>
-          <a onClick={scrollToTop}>
-            <CircleButton size={isBigScreen ? 120 : 80} rotate={-90} />
-          </a>
-        </a.div>
-      </div>
-    </a.div>
+      </a.div>
+    </div>
   )
 }
 
