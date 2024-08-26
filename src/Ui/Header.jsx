322c9c77f266a7c200148ffcc605ea712fc16f28
donsiamese <donsiamese@gmail.com>
2024-08-26 18:38:08 +0200

    logo update

--- src/Ui/Header.jsx
+++ src/Ui/Header.jsx
@@ -1,11 +1,13 @@
 import Logo3dStories from '../Svg/Logo3dStories'
 import MenuCloseButton from './MenuCloseButton'
+import { useMediaQuery } from 'react-responsive'
 
 const Header = ({ scrollToTop }) => {
+  const isBigScreen = useMediaQuery({ query: '(min-width: 640px)' })
   return (
     <>
-      <header className='Header fixed z-20 flex justify-between p-5 sm:p-10' style={{ width: 'calc(100vw - 15px)' }}>
-        <Logo3dStories width={120} scrollToTop={scrollToTop} />
+      <header className="Header fixed z-20 flex justify-between p-5 sm:p-10" style={{ width: '100%' }}>
+        <Logo3dStories width={isBigScreen ? 80 : 60} scrollToTop={scrollToTop} />
         <MenuCloseButton />
       </header>
     </>
