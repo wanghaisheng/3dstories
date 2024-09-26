d07a2e5a39d7150d8aeee7c4d9f449628dcf2c47
donsiamese <donsiamese@gmail.com>
2024-09-26 20:17:36 +0200

    improve cross browser bg compatibility

--- src/Ui/Background.jsx
+++ src/Ui/Background.jsx
@@ -1,15 +1,8 @@
-// import FlourishPattern from '../Svg/FlourishPattern'
 import './Background.css'
-// import { useMediaQuery } from 'react-responsive'
 
 const Background = () => {
-  // const isBigScreen = useMediaQuery({ query: '(min-width: 1024px)' })
   return (
     <div className="Background">
-      {/* <FlourishPattern
-        width={isBigScreen ? 500 : 200}
-        className={`${isBigScreen ? 'bottom-[3rem] right-[3rem]' : 'bottom-[1rem] right-[1rem]'} `}
-      /> */}
       <div className="filled "></div>
     </div>
   )
