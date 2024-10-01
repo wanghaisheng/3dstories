c581edf65883d429c32ee0aa1675bc44f5d2e756
donsiamese <donsiamese@gmail.com>
2024-10-01 11:31:03 +0200

    added X button

--- src/Ui/ModalWindow.jsx
+++ src/Ui/ModalWindow.jsx
@@ -1,18 +1,19 @@
+import CloseButton from './CloseButton'
+import { useMediaQuery } from 'react-responsive'
 import './ModalWindow.css'
 
 const ModalWindow = ({ isModalImage, closeModal, isModalVisible }) => {
+  const isBigScreen = useMediaQuery({ query: '(min-width: 640px)' })
   return (
     <div className={`ModalWindow ${isModalVisible ? 'open' : null}`}>
-      <button
-        className="absolute top-4 right-4"
+      <CloseButton
         onClick={() => {
-          // console.log('isModalImage', isModalImage.url)
-          console.log('CLOSE BUTTON', isModalVisible)
           closeModal()
         }}
-      >
-        Close
-      </button>
+        className="mb-2"
+        size={32}
+        style={{ position: 'absolute', right: isBigScreen ? '3rem' : '1rem', top: isBigScreen ? '2rem' : '1rem' }}
+      />
       <div className="ModalWindow-content">
         <img src={`${isModalImage?.url}`} alt={`${isModalImage?.title}`} />
         <figcaption className="mt-3">{isModalImage?.title}</figcaption>
