84932c6b86fa665619c79d7809e26a4477ebe879
donsiamese <donsiamese@gmail.com>
2024-10-14 19:23:34 +0200

    fotter

--- src/Ui/Footer.jsx
+++ src/Ui/Footer.jsx
@@ -65,7 +65,7 @@ const Footer = ({ thresholdFooter = 1 }) => {
           </a>
         </div>
       </div>
-      <span className="mt-3 md:mt-0 flex text-xs grow md:justify-end md:justify-end justify-center">
+      <span className="mt-3 md:mt-0 flex text-xs grow md:justify-end md:text-right text-center justify-center">
         © University of Applied Arts Potsdam (FHP) & Centre for Contemporary and Digital History Luxembourg (C²DH),
         2024.
       </span>
