86684453bb278569951fdd3d445b9a4ebfa143cf
donsiamese <donsiamese@gmail.com>
2024-11-27 15:19:59 +0100

    changed images and videos folder

--- src/Ui/VideoBackground.jsx
+++ src/Ui/VideoBackground.jsx
@@ -1,44 +1,40 @@
-import { useEffect, useState } from 'react';
-import { useViewportStore } from '../components/ViewportManager';
+import { useEffect, useState } from 'react'
+import { useViewportStore } from '../components/ViewportManager'
 
 const VideoBackground = ({ pathname }) => {
-  const [pauseVideo, setPauseVideo] = useState(false);
+  const [pauseVideo, setPauseVideo] = useState(false)
 
   useEffect(() => {
     if (pathname === '/') {
-      setPauseVideo(false);
-      console.log('Play video', pauseVideo);
+      setPauseVideo(false)
+      console.log('Play video', pauseVideo)
     } else {
-      setPauseVideo(true);
-      console.log('Pause video', pauseVideo);
+      setPauseVideo(true)
+      console.log('Pause video', pauseVideo)
     }
-  }, [pathname]);
+  }, [pathname])
 
-  const setBackgroundVideoReady = useViewportStore(
-    (state) => state.setBackgroundVideoReady
-  );
+  const setBackgroundVideoReady = useViewportStore(state => state.setBackgroundVideoReady)
 
   return (
     <div
-      className={`background-video ${
-        pauseVideo === false ? 'pointer-events-auto' : 'pointer-events-none'
-      }`}
+      className={`background-video ${pauseVideo === false ? 'pointer-events-auto' : 'pointer-events-none'}`}
       style={{ opacity: pauseVideo === false ? 1 : 0 }}
     >
       <video
-        src="intro.mp4"
+        src="video/intro.mp4"
         autoPlay
         loop
         muted
         playsInline
         onCanPlay={() => {
-          console.debug('[Preloader] Video ready');
-          setBackgroundVideoReady(true);
+          console.debug('[Preloader] Video ready')
+          setBackgroundVideoReady(true)
         }}
         style={{ width: '100%', height: '100%', objectFit: 'cover' }}
       ></video>
     </div>
-  );
-};
+  )
+}
 
-export default VideoBackground;
+export default VideoBackground
